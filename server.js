ec7f7e9f74574afd090545422b82b69ce775cd3b
Chisquare7 <chijiokechibuike.f@gmail.com>
2024-05-13 09:40:41 +0100

    Enabled CORS for all routes

--- server.js
+++ server.js
@@ -8,6 +8,16 @@ const fs = require("fs");
 const app = express();
 const PORT = process.env.PORT || 4050;
 
+app.use((req, res, next) => {
+    res.header(
+			"Access-Control-Allow-Origin",
+			"https://form-validation-server.onrender.com"
+		);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+})
+
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(bodyParser.json());
 
