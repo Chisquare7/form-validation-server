45fcd2e57c3fff75fb635cd6654e26758d544434
Chisquare7 <chijiokechibuike.f@gmail.com>
2024-05-13 10:49:32 +0100

    Refactor the code

--- server.js
+++ server.js
@@ -8,16 +8,6 @@ const fs = require("fs");
 const app = express();
 const PORT = process.env.PORT || 4050;
 
-app.options("*", (req, res) => {
-	res.header(
-		"Access-Control-Allow-Origin",
-		"https://form-validation-server.onrender.com"
-	);
-	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-	res.header("Access-Control-Allow-Headers", "Content-Type");
-	res.status(200).send();
-});
-
 app.use((req, res, next) => {
     res.header(
 			"Access-Control-Allow-Origin",
@@ -27,6 +17,16 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Headers", "Content-Type");
     next();
 })
+
+app.options("*", (req, res) => {
+	res.header(
+		"Access-Control-Allow-Origin",
+		"https://form-validation-server.onrender.com"
+	);
+	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+	res.header("Access-Control-Allow-Headers", "Content-Type");
+	res.status(200).send();
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(bodyParser.json());
