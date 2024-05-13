c5525ec917afa0abb4697343df2cfa80d1bc9f5e
Chisquare7 <chijiokechibuike.f@gmail.com>
2024-05-13 10:16:36 +0100

    Added OPTIONS request with CORS

--- server.js
+++ server.js
@@ -8,6 +8,16 @@ const fs = require("fs");
 const app = express();
 const PORT = process.env.PORT || 4050;
 
+app.options("*", (req, res) => {
+	res.header(
+		"Access-Control-Allow-Origin",
+		"https://form-validation-server.onrender.com"
+	);
+	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+	res.header("Access-Control-Allow-Headers", "Content-Type");
+	res.status(200).send();
+});
+
 app.use((req, res, next) => {
     res.header(
 			"Access-Control-Allow-Origin",
